309ad83bbdd5cccae660a345d7263bfe7100563c
Vortex <157533131+NicholasChavez2@users.noreply.github.com>
2026-09-05 15:04:07 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "gitops-tf-state-nicholaschavez-lab"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
