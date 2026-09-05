54a0ed2d99a01e8fbaa0646475cb3251dc94ac3f
Vortex <157533131+NCVortex2@users.noreply.github.com>
2026-09-05 16:19:50 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "vpc_id" {
-  description = "ID of the provisioned VPC"
+  description = "ID of the VPC"
   value       = aws_vpc.gitops_vpc.id
 }
 
@@ -9,6 +9,16 @@ output "subnet_id" {
 }
 
 output "security_group_id" {
-  description = "ID of the web security group"
+  description = "ID of the security group"
   value       = aws_security_group.web_sg.id
+}
+
+output "web_server_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web_server.public_ip
+}
+
+output "web_server_url" {
+  description = "Public URL to access the Nginx web server"
+  value       = "http://${aws_instance.web_server.public_ip}"
 }
