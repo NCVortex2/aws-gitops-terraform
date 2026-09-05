9e18250c4b8ddadfa8b69c701f82b57a3c855ca4
Vortex <157533131+NicholasChavez2@users.noreply.github.com>
2026-09-04 21:13:38 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,14 @@
+output "vpc_id" {
+  description = "ID of the provisioned VPC"
+  value       = aws_vpc.gitops_vpc.id
+}
 
+output "subnet_id" {
+  description = "ID of the public subnet"
+  value       = aws_subnet.public_subnet_1.id
+}
+
+output "security_group_id" {
+  description = "ID of the web security group"
+  value       = aws_security_group.web_sg.id
+}
