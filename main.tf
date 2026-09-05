66ad444ea36e47f99c411adbc72eb53c36ac634b
Vortex <157533131+NCVortex2@users.noreply.github.com>
2026-09-05 16:51:09 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,22 +72,12 @@ resource "aws_security_group" "web_sg" {
   description = "Allow inbound HTTP"
   vpc_id      = aws_vpc.gitops_vpc.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "gitops-web-sg"
+  }
+
+  lifecycle {
+    ignore_changes = [ingress, egress]
   }
 }
 
